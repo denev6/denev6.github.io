f839ff75b599bb0dd3fbb7a1e12b2d431b110dfc
denev6 <sungjin.code@gmail.com>
2025-06-02 14:03:33 +0900

    update dacon-bias

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250525.111301';
+const cacheName = 'chirpy-20250602.140318';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
