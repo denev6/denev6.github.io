3fc46c988c1ee148a60642869a261450febbb32f
denev6 <sungjin.code@gmail.com>
2025-10-20 01:05:17 +0900

    update: add ACE in agent memory

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20251020.005158';
+const cacheName = 'chirpy-20251020.010430';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
