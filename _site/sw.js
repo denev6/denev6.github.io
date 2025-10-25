901f11bc16663ca7f53cd70ee17b31b3d736c9bf
denev6 <sungjin.code@gmail.com>
2025-10-26 01:41:04 +0900

    post: hrm

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20251021.004421';
+const cacheName = 'chirpy-20251026.014052';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
