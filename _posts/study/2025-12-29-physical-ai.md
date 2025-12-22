e726fa1305a6358467d16790111fd03c8a4a755e
denev6 <sungjin.code@gmail.com>
2025-12-23 00:14:11 +0900

    update: about and add memoir-25

---
+++ _posts/study/2025-12-29-physical-ai.md
@@ -0,0 +1,9 @@
+---
+title: Physical 최신 동향 살펴보기
+category: Study
+tags: [Agent, AI, CV, RL]
+toc: true 
+math: true
+img_path: /assets/posts/physical-ai/
+---
+
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20251221.022119';
+const cacheName = 'chirpy-20251223.001321';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
