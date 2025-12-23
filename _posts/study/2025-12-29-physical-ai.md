06af91717e9b3af9b775fb004490682e9d6da75b
denev6 <sungjin.code@gmail.com>
2025-12-24 00:03:04 +0900

    add imgs

--- _posts/study/2025-12-29-physical-ai.md
+++
@@ -1,9 +0,0 @@
----
-title: Physical 최신 동향 살펴보기
-category: Study
-tags: [Agent, AI, CV, RL]
-toc: true 
-math: true
-img_path: /assets/posts/physical-ai/
----
-
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20251223.001321';
+const cacheName = 'chirpy-20251224.000032';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
