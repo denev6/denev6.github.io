a4521a07db1012ba9708f1adc3fe56f457039f3a
denev6 <sungjin.code@gmail.com>
2025-12-27 22:20:27 +0900

    post: general-vla

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20251224.162306';
+const cacheName = 'chirpy-20251227.222013';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
