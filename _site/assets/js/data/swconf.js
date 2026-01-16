a23f9088b6c18b51081587e0bf8dbf0c4133ea1e
denev6 <sungjin.code@gmail.com>
2026-01-16 17:42:51 +0900

    chore: fix typo

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1768543960',resources: [
+    cacheName: 'chirpy-1768552897',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
